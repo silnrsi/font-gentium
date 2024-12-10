00e289b74557ad8d298cb2f7e8bedc4641061c20
nrsiward <nrsiward@users.noreply.github.com>
2024-12-09 21:42:29 -0600

    [RFS-829] Added Greek support to c2sc feature and updated ftml tests

--- tools/makegrkrecompftml.py
+++ tools/makegrkrecompftml.py
@@ -1,8 +1,10 @@
 
 #!/usr/bin/python3
 '''
-Make ftml tests for Greek combinataions based on content of grk_compose.feax
-Also test smcp feature on lower case sequences for glyphs which are produced by recomposition. 
+Makes ftml tests for Greek combinations based on content of grk_compose.feax
+Generates a test for each rule in the recomposition lookup
+and a test if the first two glyphs match a decomposition (using the USV of the composed glyph)
+Also generates tests for smcp and c2sc features on lower or upper case sequences
   see smcp.ftml, etc for glyphs not so produced (eg GrSmAlpha)
 
 Hacky script intended for rare usage. There's no cli, so edit strings below if needed.
@@ -37,6 +39,7 @@ ftml_style_template = '''
     </test>
 '''[1:]
 smcp_stylename="smcp_1"
+c2sc_stylename="c2sc_1"
 
 ftml_start = '''
 <?xml version="1.0" encoding="UTF-8"?>
@@ -58,6 +61,7 @@ ftml_start = '''
     <fontsrc label="GIv6">url(../references/v6200/GentiumPlus-Italic.ttf)</fontsrc>
     <styles>
       <style feats="'smcp' 1" name="smcp_1"/>
+      <style feats="'c2sc' 1" name="c2sc_1"/>
     </styles>
     <title>Greek combinations</title>
   </head>
@@ -74,7 +78,7 @@ ftml_end = '''
 </ftml>
 '''[1:]
 
-# build dict to map glyph names to unicode values
+# build dict to map glyph names to unicode values from UFO
 name_to_unicode = {}
 ufo_f = ufo.Ufont(ufo_fn)
 for g_name in ufo_f.deflayer:
@@ -92,13 +96,15 @@ feax_f.close()
 ftml_f = open(ftml_fn, "w")
 ftml_f.write(ftml_start)
 
+# Only processes feax until the small cap lookup is reached
+# Assumes two-to-one mappings are decomposition and one-to-two mappings are recomposition
 compose_to_unicode = {}
 for l in feax:
     field = re.split(r"\W+", l) # first field of matching lines will contain white space
     glyph_lst = []
     if field[1] == "sub" and field[3] == "by":
         # extract two-to-one mapping from base + diac to encoded precomposed base w diac
-        # based on decomposition rules
+        # based on decomposition rules; used to test USV with precomposed glyph
         # for example: sub GrCapAlphaWMacron by GrCapAlpha CombMacron;
         unicode = name_to_unicode[field[2]]
         compose_to_unicode[field[4] + "-" + field[5]] = unicode
@@ -120,17 +126,20 @@ for l in feax:
 
     # write ftml lines to output file
     unicode_lst = [name_to_unicode[x] for x in glyph_lst]
-    test_label = "+".join(unicode_lst) # joing USVs with '+'
+    test_label = "+".join(unicode_lst) # join USVs with '+'
     test_string = "".join([f"\\u{x:0>6}" for x in unicode_lst]) # USVs with six digits and leading zeroes
     ftml_test_str = ftml_template.format(test_label, test_string)
     ftml_f.write(ftml_test_str)
     if chr(int(unicode_lst[0], 16)).islower(): # add smcp test for lower case glyphs
         ftml_test_str = ftml_style_template.format(test_label, smcp_stylename, test_string)
         ftml_f.write(ftml_test_str)
-    if glyph_lst[0] + "-" + glyph_lst[1] in compose_to_unicode:
-        # write line to test codes with precomposed base w diac
+    if chr(int(unicode_lst[0], 16)).isupper(): # add c2sc test for upper case glyphs
+        ftml_test_str = ftml_style_template.format(test_label, c2sc_stylename, test_string)
+        ftml_f.write(ftml_test_str)
+    if glyph_lst[0] + "-" + glyph_lst[1] in compose_to_unicode: # first two glyphs have a composed form
+        # write line to test seqs with precomposed base w diac
         unicode_lst = [compose_to_unicode[glyph_lst[0] + "-" + glyph_lst[1]]]
-        unicode_lst.extend([name_to_unicode[x] for x in glyph_lst[2:]])
+        unicode_lst.extend([name_to_unicode[x] for x in glyph_lst[2:]]) # add diacs after composed form
         test_label = "+".join(unicode_lst)
         test_string = "".join([f"\\u{x:0>6}" for x in unicode_lst])
         ftml_test_str = ftml_template.format(test_label, test_string)
@@ -138,10 +147,14 @@ for l in feax:
         if chr(int(unicode_lst[0], 16)).islower():
             ftml_test_str = ftml_style_template.format(test_label, smcp_stylename, test_string)
             ftml_f.write(ftml_test_str)
+        if chr(int(unicode_lst[0], 16)).isupper():
+            ftml_test_str = ftml_style_template.format(test_label, c2sc_stylename, test_string)
+            ftml_f.write(ftml_test_str)
 
 # add tests for certain glyphs that are not accessed through recomposition
 #  derived by examinging lookup grk_sc1_sub in grk_compose.feax for non GrSm* glyphs
 #  these are also tested in smcp.ftml tests
+# no tests added for similar non GrCap* glyphs
 ftml_f.write(ftml_smcp_special)
 smcp_test_lst = ["GrDottedLunateSigma", "GrLunateSigma", "GrRevDottedLunateSigma",
                   "GrRevLunateSigma", "GrYot"]
